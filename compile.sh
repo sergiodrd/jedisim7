133c7994baa39fb6385b928c93040a68a134e561
sergio <sergiod.roddj@gmail.com>
2024-07-17 14:22:09 -0400

    initial code commit

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm -rf build
+mkdir build
+cd build
+cmake ..
+cmake --build . -- VERBOSE=1
+cd ..
---
+++ tests/tests.c
@@ -0,0 +1,14 @@
+#include "unity.h"
+
+void setUp() {}
+void tearDown() {}
+
+void parse_config_file_should_populate_properly() {
+  // TODO
+}
+
+int main(int argc, char *argv[]) {
+  UNITY_BEGIN();
+  RUN_TEST(parse_config_file_should_populate_properly);
+  return UNITY_END();
+}
